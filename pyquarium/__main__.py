4c2dae71653a9c55d1d2f554c7cc12f2946a71e8
Alexander Walters <123330507+awinstonw@users.noreply.github.com>
2025-01-03 20:54:51 -0800

    cli improvements

--- pyquarium/__main__.py
+++ pyquarium/__main__.py
@@ -5,15 +5,28 @@ from pyquarium import render_aquarium
 
 
 def _get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(prog='pyquarium')
-    parser.add_argument('-f', '--fish', nargs='?', default=8, type=int)
-    parser.add_argument('-b', '--bubblers', nargs='?', default=3, type=int)
-    parser.add_argument('-k', '--kelp', nargs='?', default=4, type=int)
-    parser.add_argument('fps', nargs='?', default=6, type=int)
+    parser = argparse.ArgumentParser(
+        prog='pyquarium',
+        description='ascii art aquarium for your terminal',
+        epilog='<https://github.com/awinstonw/pyquarium/>'
+    )
+    parser.add_argument('-f', '--fish', nargs='?', default=8, type=int,
+                        help='default=8, max=30, min=0')
+    parser.add_argument('-b', '--bubblers', nargs='?', default=3, type=int,
+                        help='default=3, max=15, min=0')
+    parser.add_argument('-k', '--kelp', nargs='?', default=4, type=int,
+                        help='default=4, max=15, min=0')
+    parser.add_argument('fps', nargs='?', default=6, type=int,
+                        help='default=6, max=45, min=1')
     return parser.parse_args()
 
 
 args= _get_args()
+# Reasonable limits for most conditions.
+args.fish = min(30, args.fish)
+args.bubblers = min(15, args.bubblers)
+args.kelp = min(15, args.kelp)
+args.fps = min(45, max(1, args.fps))
 try:
     render_aquarium(args.fish, args.bubblers, args.kelp, args.fps)
 except KeyboardInterrupt:
